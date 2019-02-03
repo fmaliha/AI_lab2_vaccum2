11983a5ea2384be7f95862ef03e680af61f14e0f
fmaliha <43738990+fmaliha@users.noreply.github.com>
2019-02-03 14:09:00 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+string simple_reflex_agent(string percept)
+{
+ string p=percept;
+  if()
+
+
+
+
+}
+
+string interpret_input(string percept)
+{
+
+}
+
+string rule_match(string state,string rule)
+{
+
+}
+
+string rule_action(string rule)
+{
+
+
+}
+int main()
+{
+
+    return 0;
+}
